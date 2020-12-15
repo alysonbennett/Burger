56656a769994cdda8acee28c3d527ae479d57226
alysonbennett <alysonbennett@gmai.com>
2020-12-15 13:07:15 -0500

    added helper functions to orm

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,34 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
+// Helper functions for SQL syntax
+// Creates an array of question marks
+function createQuestionMarks(num) {
+     var arr = [];
+   
+     for (var i = 0; i < num; i++) {
+       arr.push("?");
+     }
+   
+     return arr.toString();
+   }
+
+// Converts object key/value pairs to SQL syntax
+function objToSql(ob) {
+     var arr = [];
+   
+     for (var key in ob) {
+       var value = ob[key];
+       if (Object.hasOwnProperty.call(ob, key)) {
+         if (typeof value === "string" && value.indexOf(" ") >= 0) {
+           value = "'" + value + "'";
+         }
+         arr.push(key + "=" + value);
+       }
+     }
+   
+     return arr.toString();
+   }
 
 // Object for all SQL statement functions
 var orm = {
