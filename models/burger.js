730a4f2349106558fb73540ac5ae01ff46a87247
alysonbennett <alysonbennett@gmai.com>
2020-12-15 13:13:31 -0500

    updated model/burger.js to export database functions

--- models/burger.js
+++ models/burger.js
@@ -1,11 +1,30 @@
 var orm = require("../config/orm.js");
 
+var burger = {
+    selectAll: function(cb) {
+      orm.selectAll("burgers", function(res) {
+        cb(res);
+      });
+    },
 
-// CRUD
+    insertOne: function(cols, vals, cb) {
+      orm.insertOne("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
 
+    updateOne: function(objColVals, condition, cb) {
+      orm.updateOne("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    },
 
-
-
-
-
-// module.exports = burger;
+    deleteOne: function(condition, cb) {
+      orm.deleteOne("burgers", condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+  
+  // Export the database functions for the controller (catsController.js).
+  module.exports = burger;
