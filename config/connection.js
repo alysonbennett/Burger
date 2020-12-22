92b65abc098da1308227f40ada4c7a312cd69ee3
alysonbennett <alysonbennett@gmai.com>
2020-12-21 19:14:37 -0500

    fixed heroku connection

--- config/connection.js
+++ config/connection.js
@@ -2,24 +2,30 @@
 var util = require("util");
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "rootroot",
     database: "burgers_db"
   });
+}
 
-  // Make connection
-  connection.connect(function(err) {
-    if (err) {
-      console.error("error connection: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId); 
-  });
+// Make connection
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connection: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
-  connection.query = util.promisify(connection.query);
+connection.query = util.promisify(connection.query);
 
-  // Export connection for ORM to use
-  module.exports = connection;
+// Export connection for ORM to use
+module.exports = connection;
